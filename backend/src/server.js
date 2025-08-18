8847997e5721120845917cca0362676cfe77fc7d
mohammad-umair32 <mohammadumair.720855@gmail.com>
2025-08-18 15:02:11 +0530

    add fetch favorites endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
-import { and,eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
@@ -53,10 +53,26 @@ app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
         )
       );
 
-      res.status(200).json({ message: "Favorite removed successfully" });
-
+    res.status(200).json({ message: "Favorite removed successfully" });
   } catch (error) {
     console.log("Error removing a favorite:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.get("/api/favorites/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+    res.status(200).json(userFavorites);
+
+  } catch (error) {
+    console.log("Error fetching a favorite:", error);
     res.status(500).json({ error: "Something went wrong" });
   }
 });
